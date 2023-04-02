e9e5dbe3ac2c5707e012f3629464a951fbae11b8
papunatsiskadze <papunaciskadze@gmail.com>
2023-04-02 18:51:01 +0400

    done commit

---
+++ Js2.js
@@ -0,0 +1,70 @@
+/*
+let userIsloggedIn=true;
+let videoIsCommentable=true;
+
+const userCanComment=userIsloggedIn && videoIsCommentable;
+console.log(userCanComment);
+*/
+
+/*
+let userIsloggedIn=true;
+let userIsBlocked=false;
+
+const userCanPost=userIsloggedIn || userIsBlocked;
+console.log(userCanPost);
+*/
+
+/*
+let balance=305;
+let userCartTotal=300;
+
+const paymentComplete=balance>=userCartTotal;
+console.log(paymentComplete);
+*/
+
+/*
+let userIsAdmin=true;
+let userIsModerator=true;
+let userIsCommentAuthor=true;
+
+const userCanDelete=userIsAdmin || userIsModerator || userIsCommentAuthor;
+console.log(userCanDelete);
+*/
+
+
+/*
+let presidentKey=true;
+let primeMinisterKey=false;
+let generalOfArmiesKey=true;
+let masterKey=true;
+
+const bombWillLaunch=(presidentKey && primeMinisterKey && generalOfArmiesKey) || masterKey;
+console.log(bombWillLaunch);
+*/
+
+/*
+const userPinCode=8888;
+const curretntPinCode=8888;
+const newPinCode=5555;
+const retypeNewPinCode=5555;
+
+const pinChangeComplete=(userPinCode == curretntPinCode) && (newPinCode == retypeNewPinCode);
+console.log(pinChangeComplete);
+*/
+
+/*
+let health=20;
+let lives=3;
+
+const gameOver=(health==0) && (lives==0);
+console.log(gameOver);
+*/
+
+
+let capsuleCount=;
+let capsulePrice=;
+let userBalance=;
+let creditCardStatus=;
+ 
+const paymentComplete=;
+console.log(paymentComplete);
